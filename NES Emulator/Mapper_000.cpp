bb40bb82ac5042c0150a8f8781f8df1c674d8654
Akshaj <akshaj.kanumuri@gmail.com>
2024-06-17 14:22:06 -0700

    update

--- NES Emulator/Mapper_000.cpp
+++ NES Emulator/Mapper_000.cpp
@@ -11,20 +11,38 @@ Mapper_000::~Mapper_000()
 
 bool Mapper_000::cpuMapRead(uint16_t addr, uint32_t& mapped_addr)
 {
+    if (addr >= 0x8000 && addr <= 0xFFFF)
+    {
+        mapped_addr = addr & (prgBanks > 1 ? 0x7FFF : 0x3FFF);
+        return true;
+    }
+
     return false;
 }
 
 bool Mapper_000::cpuMapWrite(uint16_t addr, uint32_t& mapped_addr)
 {
+    if (addr >= 0x8000 && addr <= 0xFFFF)
+    {
+        return true;
+    }
+
     return false;
 }
 
 bool Mapper_000::ppuMapRead(uint16_t addr, uint32_t& mapped_addr)
 {
+    if (addr >= 0x0000 && addr <= 0x1FFF)
+    {
+        return true;
+    }
+
     return false;
 }
 
 bool Mapper_000::ppuMapWrite(uint16_t addr, uint32_t& mapped_addr)
 {
+    if (addr >= 0x0000 && addr <= 0x1FFF)
+
     return false;
 }
